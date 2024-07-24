40f43d03e95da807f27e748a765cc0f94d0c92fb
skylarpie <kylaepineda1416@gmail.com>
2024-07-24 16:32:03 -0400

    Landscape mode modifications

--- lib/page/airplane_page.dart
+++ lib/page/airplane_page.dart
@@ -5,8 +5,8 @@ import 'package:i_can_fly/utils/theme-color.dart';
 import '../entity/airplane.dart';
 
 /**
- * AirplanePage is a stateful widget that displays a list of airplanes.
- * Users can view the list of airplanes, and add or edit airplane details.
+ * AirplanePage displays a list of airplanes and allows navigation to the ManageAirplanePage
+ * for adding or editing airplane details.
  */
 class AirplanePage extends StatefulWidget {
   const AirplanePage({super.key});
@@ -16,46 +16,106 @@ class AirplanePage extends StatefulWidget {
 }
 
 class _AirplanePageState extends State<AirplanePage> {
-  late Future<List<Airplane>> _airplanes; // Future that will hold the list of airplanes
+  late Future<List<Airplane>> _airplanes;
+  Airplane? _selectedAirplane;
 
   @override
   void initState() {
     super.initState();
-    _airplanes = _loadAirplanes(); // Load airplanes when the widget initializes
+    _airplanes = _loadAirplanes();
   }
 
-  /**
-   * Loads the list of airplanes from the database.
-   *
-   * @return A Future that resolves to a list of Airplane objects.
-   */
   Future<List<Airplane>> _loadAirplanes() async {
     try {
       final database = await AppDatabase.getInstance();
       final dao = database.airplaneDao;
-      return await dao.findAllAirplanes(); // Retrieve all airplanes from the DAO
+      return await dao.findAllAirplanes();
     } catch (e) {
       // Print or log the error for debugging
       print("Error loading airplanes: $e");
-      return []; // Return an empty list in case of an error
+      return [];
     }
   }
 
-  /**
-   * Navigates to the ManageAirplanePage to add or edit an airplane.
-   *
-   * @param context The build context.
-   * @param airplane The airplane to be edited (null if adding a new airplane).
-   */
   void _navigateToManagePage(BuildContext context, [Airplane? airplane]) {
-    Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (context) => ManageAirplanePage(
-          airplane: airplane,
-          isEditMode: airplane != null, // Determine if the page is in edit mode
+    if (MediaQuery.of(context).orientation == Orientation.landscape &&
+        MediaQuery.of(context).size.width > 720) {
+      setState(() {
+        _selectedAirplane = airplane;
+      });
+    } else {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => ManageAirplanePage(
+            airplane: airplane,
+            isEditMode: airplane != null,
+          ),
         ),
-      ),
+      );
+    }
+  }
+
+  Widget _buildLandscapeLayout() {
+    return Row(
+      children: [
+        Expanded(flex: 1, child: _buildAirplaneList()),
+        if (_selectedAirplane != null)
+          Expanded(
+            flex: 2,
+            child: ManageAirplanePage(
+              airplane: _selectedAirplane,
+              isEditMode: true,
+            ),
+          ),
+      ],
+    );
+  }
+
+  Widget _buildAirplaneList() {
+    return FutureBuilder<List<Airplane>>(
+      future: _airplanes,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        } else if (snapshot.hasError) {
+          return Center(child: Text('Error: ${snapshot.error}'));
+        } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+          return const Center(child: Text('No airplanes found.'));
+        } else {
+          final airplanes = snapshot.data!;
+          return ListView.builder(
+            itemCount: airplanes.length,
+            itemBuilder: (context, index) {
+              final airplane = airplanes[index];
+              return GestureDetector(
+                onTap: () => _navigateToManagePage(context, airplane),
+                child: Container(
+                  margin: const EdgeInsets.symmetric(
+                      vertical: 8.0, horizontal: 16.0),
+                  padding: const EdgeInsets.all(16.0),
+                  decoration: BoxDecoration(
+                    color: Color(CTColor.DarkTeal.colorValue),
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                  child: Row(
+                    children: [
+                      Icon(Icons.airplane_ticket, color: Colors.white),
+                      SizedBox(width: 16.0),
+                      Expanded(
+                        child: Text(
+                          '${airplane.type}    ${airplane.capacity} Passengers',
+                          style: TextStyle(fontSize: 16.0, color: Colors.white),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            },
+          );
+        }
+      },
     );
   }
 
@@ -67,55 +127,22 @@ class _AirplanePageState extends State<AirplanePage> {
           "See All Your Airplanes Here!",
           style: TextStyle(color: Colors.white),
         ),
-        backgroundColor: Color(CTColor.Teal.colorValue), // Set the AppBar color
+        backgroundColor: Color(CTColor.Teal.colorValue),
       ),
-      body: FutureBuilder<List<Airplane>>(
-        future: _airplanes,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator()); // Show loading indicator while waiting
-          } else if (snapshot.hasError) {
-            return Center(child: Text('Error: ${snapshot.error}')); // Show error message if an error occurs
-          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-            return const Center(child: Text('No airplanes found.')); // Show message if no airplanes are found
+      body: OrientationBuilder(
+        builder: (context, orientation) {
+          if (orientation == Orientation.landscape &&
+              MediaQuery.of(context).size.width > 720) {
+            return _buildLandscapeLayout();
           } else {
-            final airplanes = snapshot.data!;
-            return ListView.builder(
-              itemCount: airplanes.length,
-              itemBuilder: (context, index) {
-                final airplane = airplanes[index];
-                return GestureDetector(
-                  onTap: () => _navigateToManagePage(context, airplane), // Navigate to edit airplane on tap
-                  child: Container(
-                    margin: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-                    padding: const EdgeInsets.all(16.0),
-                    decoration: BoxDecoration(
-                      color: Color(CTColor.DarkTeal.colorValue),
-                      borderRadius: BorderRadius.circular(8.0),
-                    ),
-                    child: Row(
-                      children: [
-                        Icon(Icons.airplane_ticket, color: Colors.white),
-                        SizedBox(width: 16.0),
-                        Expanded(
-                          child: Text(
-                            '${airplane.type}    ${airplane.capacity} Passengers', // Display airplane type and capacity
-                            style: TextStyle(fontSize: 16.0, color: Colors.white),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                );
-              },
-            );
+            return _buildAirplaneList();
           }
         },
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () => _navigateToManagePage(context), // Navigate to add airplane on button press
+        onPressed: () => _navigateToManagePage(context),
         child: const Icon(Icons.add),
-        backgroundColor: Color(CTColor.Teal.colorValue), // Set the FAB color
+        backgroundColor: Color(CTColor.Teal.colorValue),
       ),
     );
   }
