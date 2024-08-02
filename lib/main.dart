e831ca5de2a480fc52cbc99dbb0def68f4f45649
Loy Yee Ko <koloyyee@gmail.com>
2024-08-01 22:04:36 -0400

    overriding main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:i_can_fly/dao/flight_dao.dart';
-import 'package:i_can_fly/db/database.dart';
-import 'package:i_can_fly/entity/customer.dart';
 import 'package:i_can_fly/page/admin/admin_login.dart';
 import 'package:i_can_fly/page/admin/admin_reg.dart';
 import 'package:i_can_fly/page/airplane/airplane_page.dart';
 import 'package:i_can_fly/page/airplane/manage_airplane.dart';
-import 'package:i_can_fly/page/customer/customer_home_page.dart';
 import 'package:i_can_fly/page/customer/customer_list.dart';
 import 'package:i_can_fly/page/customer/customer_login_page.dart';
 import 'package:i_can_fly/page/customer/customer_register_page.dart';
-import 'package:i_can_fly/page/customer/edit_customer_page.dart';
 import 'package:i_can_fly/page/flight/flight_page.dart';
 import 'package:i_can_fly/page/flight/add_flight.dart';
-import 'package:i_can_fly/page/airplane/airplane_page.dart';
 import 'package:i_can_fly/page/welcome.dart';
 import 'package:i_can_fly/utils/theme-color.dart';
-import 'dart:async';
-
 import 'package:i_can_fly/page/reservation/reservation_list.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-/// The main application widget.
-///
-/// This widget sets up the MaterialApp with routes and theme.
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -46,63 +35,16 @@ class MyApp extends StatelessWidget {
       routes: {
         "/welcome": (context) => const WelcomePage(),
         "/": (context) => const HomePage(),
-
-        /// Route for the Customer Home page, requires a Customer object as an argument.
-        "/customer-home": (context) {
-          final customer = ModalRoute.of(context)!.settings.arguments as Customer;
-          return CustomerHomePage(customer: customer);
-        },
         "/flights": (context) => const FlightsPage(),
         "/admin-login": (context) => const AdminLoginPage(),
         "/admin-register": (context) => const AdminRegisterPage(),
         "/add-flight": (context) => const AddFlightPage(),
-
-        /// Route for the Customer Login page.
-        /// This route initializes the AppDatabase and passes it to the CustomerLoginPage.
-        "/customer-login": (context) {
-          final databaseFuture = $FloorAppDatabase.databaseBuilder('app_database.db').build();
-          return FutureBuilder<AppDatabase>(
-            future: databaseFuture,
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.done) {
-                return CustomerLoginPage(database: snapshot.data!);
-              } else {
-                return const CircularProgressIndicator();
-              }
-            },
-          );
-        },
+        "/customer-login": (context) => const CustomerLoginPage(),
         "/customer-register": (context) => const CustomerRegisterPage(),
         "/airplanes": (context) => const AirplanePage(),
+        "/manage-airplane": (context) => const ManageAirplanePage(isEditMode: false),
         "/customers": (context) => const CustomerListPage(),
-
-        /// Route for editing a customer profile.
-        /// Requires a Customer object as an argument.
-        "/edit-customer": (context) {
-          final customer = ModalRoute.of(context)!.settings.arguments as Customer;
-          return EditCustomerPage(customer: customer);
-        },
-
-        /// Route for the Reservations page.
-        /// Requires a FlightDao object as an argument.
-        "/reservations": (context) {
-          final flightDao = ModalRoute.of(context)!.settings.arguments as FlightDao;
-          return ReservationListPage(flightDao: flightDao);
-        },
-      },
-
-      /// Function to handle dynamic route generation.
-      /// For the /edit-customer route, this method is used to create a MaterialPageRoute.
-      onGenerateRoute: (settings) {
-        if (settings.name == "/edit-customer") {
-          final customer = settings.arguments as Customer;
-          return MaterialPageRoute(
-            builder: (context) {
-              return EditCustomerPage(customer: customer);
-            },
-          );
-        }
-        return null;
+        "/reservations": (context) => const ReservationListPage(),
       },
       restorationScopeId: "app",
     );
@@ -119,41 +61,80 @@ class HomePage extends StatelessWidget {
         title: const Text("Welcome to I Can Fly"),
         backgroundColor: Color(CTColor.Teal.colorValue),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+      body: ListView(
+        padding: const EdgeInsets.all(70.0),
+        children: [
+          _buildButton(
+            title: 'Flights List',
+            imagePath: 'images/flights.jpg',
+            context: context,
+            routeName: '/admin-login',
+          ),
+          _buildButton(
+            title: 'Airplanes List',
+            imagePath: 'images/airplane.jpg',
+            context: context,
+            routeName: '/airplanes',
+          ),
+          _buildButton(
+            title: 'Airlines List',
+            imagePath: 'images/airline.jpg',
+            context: context,
+            routeName: '/airlines',
+          ),
+          _buildButton(
+            title: 'Customer List',
+            imagePath: 'images/Customers.jpg',
+            context: context,
+            routeName: '/customers',
+          ),
+          _buildButton(
+            title: 'Reservation Page',
+            imagePath: 'images/Reservation.jpeg',
+            context: context,
+            routeName: '/reservations',
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildButton({
+    required String title,
+    required String imagePath,
+    required BuildContext context,
+    required String routeName,
+  }) {
+    const double imageHeight = 275.0; // Define height here
+    const double opacity = 0.50; // Set desired opacity (0.0 to 1.0)
+
+    return GestureDetector(
+      onTap: () {
+        Navigator.pushNamed(context, routeName);
+      },
+      child: Container(
+        height: imageHeight, // Set the defined height
+        margin: const EdgeInsets.symmetric(vertical: 8.0),
+        child: Stack(
+          fit: StackFit.expand,
           children: [
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, "/flights");
-              },
-              child: const Text("Flights List"),
+            Opacity(
+              opacity: opacity, // Adjust opacity here
+              child: Image.asset(
+                imagePath,
+                fit: BoxFit.cover,
+              ),
             ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, "/airplanes");
-              },
-              child: const Text("Airplanes List"),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, "/airlines");
-              },
-              child: const Text("Airlines List"),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(
-                    context, "/customers"); // Navigate to CustomerListPage
-              },
-              child: const Text("Customer List"),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(
-                    context, "/reservation");
-              },
-              child: const Text("Reservation Page"),
+            Center(
+              child: Text(
+                title,
+                style: TextStyle(
+                  color: Color(CTColor.BlackTeal.colorValue), // Text color set to DarkTeal
+                  fontSize: 35, // Change font size here
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Montserrat', // Apply Montserrat font
+                ),
+              ),
             ),
           ],
         ),
