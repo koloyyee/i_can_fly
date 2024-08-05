cce0b98ee7e49c1fb65f9b485ad933943fdb0c25
Loy Yee Ko <koloyyee@gmail.com>
2024-08-05 08:16:40 -0400

    udpate: dml

--- lib/db/seed/airlines_202407221736.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO airlines (code,companyName) VALUES
-	 ('AA','American Airlines'),
-	 ('UA','United Airlines'),
-	 ('DL','Delta Air Lines'),
-	 ('SWA','Southwest Airlines'),
-	 ('BA','British Airways');
