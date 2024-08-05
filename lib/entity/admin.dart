8522fb6cca3c711775aebbe2f15b90373ed59e9e
Loy Yee Ko <koloyyee@gmail.com>
2024-08-05 08:30:23 -0400

    doc: adding doc

--- lib/entity/admin.dart
+++ lib/entity/admin.dart
@@ -1,6 +1,21 @@
 
 import 'package:floor/floor.dart';
 
+/// Admin Entity class that represents the admin table in the database
+/// - id as primary key
+/// - email as the email of the admin
+/// - password as the password of the admin
+/// - createdAt as the date and time the admin was created
+/// 
+/// usage:
+/// 
+/// ```dart
+/// var newAdmin = Admin (
+/// email: 'admin@example.com', 
+/// password: 'password', 
+/// createdAt: DateTime.now()
+/// );
+/// ```
 @Entity(tableName: "admins")
 class Admin{
   @PrimaryKey(autoGenerate: true)
